ecb25d3d019efeba418917094c9aaaadaa534ff3
Lilian Torres <lifalto@gmail.com>
2019-10-24 14:10:09 -0400

    Ya guarda y busca Materia

--- carrera.php
+++ carrera.php
@@ -22,19 +22,58 @@ function buscarCarrera(){
     echo $data;
 };
 
-function guardarCarrera(){
-    //echo 'guardar carrera1';
-$postdata = file_get_contents ("php://input");
-//aqui esta como un mapa, arreglo
-$data = json_decode($postdata, true);
-//echo ' La carrera '.$data[nombre];
 
-$cn = getConexion();
+function buscarMateria(){
+    #echo "buscar Materia";
+       $cn = getConexion();
 
-$stm = $cn->prepare("INSERT INTO carrera (nombre) VALUES (:nombre)");
-$stm->bindParam(":nombre", $data[nombre]);
-$data = $stm->execute();
-echo 'guardar carrera';
+    $stm = $cn->query("SELECT * FROM materia");
+
+    $rows = $stm-> fetchAll(PDO::FETCH_ASSOC);
+
+    $data =[];
+
+    foreach ($rows as $row)
+    {$data[] = [
+        "id"=> $row["id"],
+        "nombre"=> $row["nombre"],
+        "creditos" => $row["creditos"]
+        ];
+
+    }
+    header("Content-Type: application/json", true);
+    $data = json_encode($data);
+    echo $data;
+};
+
+
+
+function guardarCarrera()
+{     //echo 'guardar carrera1';
+    $postdata = file_get_contents ("php://input");
+    //aqui esta como un mapa, arreglo
+    $data = json_decode($postdata, true);
+    //echo ' La carrera '.$data[nombre];
+    $cn = getConexion();
+    $stm = $cn->prepare("INSERT INTO carrera (nombre) VALUES (:nombre)");
+    $stm->bindParam(":nombre", $data[nombre]);
+    $data = $stm->execute();
+    echo 'guardar carrera';
+};
+
+
+function guardarMateria()
+{     //echo 'guardar Materia';
+    $postdata = file_get_contents ("php://input");
+    //aqui esta como un mapa, arreglo
+    $data = json_decode($postdata, true);
+    //echo ' La carrera '.$data[nombre];
+    $cn = getConexion();
+    $stm = $cn->prepare("INSERT INTO materia (nombre, creditos) VALUES (:nombre, :creditos)");
+    $stm->bindParam(":nombre", $data[nombre]);
+    $stm->bindParam(":creditos", $data[creditos]);
+    $data = $stm->execute();
+    echo 'guardar materia';
 };
 
 $method = $_SERVER['REQUEST_METHOD'];
@@ -47,9 +86,11 @@ switch($method){
         buscarCarrera();
         break;
     case 'DELETE':
-
+        buscarMateria();
+        break;
     case 'PUT':
-
+        guardarMateria();
+        break;
     default: 
     echo 'TO BE IMPLEMENTED';
 
